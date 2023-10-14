dce1bb5ac34a88c3337400ae9550b76932755fcd
NF RIDOY <nfridoy@gmail.com>
2023-10-15 03:33:47 +0600

    Feat: addCoffee res Mob

--- src/components/AddCoffee/AddCoffee.jsx
+++ src/components/AddCoffee/AddCoffee.jsx
@@ -43,43 +43,43 @@ export default function AddCoffee() {
             <div className="bg-[#F4F3F0] p-10 space-y-10">
                 <h1 className="text-5xl">Add New Coffee</h1>
                 <p>
-                    It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.
+                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.
                 </p>
-                <form className="p-10" onSubmit={handleAddCoffee}>
-                    <div className="grid grid-cols-2 gap-y-6">
-                        <div className="flex flex-col  items-start w-full">
+                <form className="md:p-10" onSubmit={handleAddCoffee}>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-y-6">
+                        <div className="flex flex-col items-start w-full gap-3">
                             <label>Name</label>
-                            <input type="text" className="p-4 rounded-md w-11/12 mr-0 " name="Name" placeholder="Name" />
+                            <input type="text" className="p-4 rounded-md w-full md:w-11/12  mr-0 " name="Name" placeholder="Name" />
                         </div>
-                        <div className="flex flex-col  items-start w-full">
+                        <div className="flex flex-col items-start w-full gap-3">
                             <label>Chef</label>
-                            <input type="text" className="p-4 rounded-md w-11/12 mr-0" name="Chef" placeholder="Chef" />
+                            <input type="text" className="p-4 rounded-md w-full md:w-11/12  mr-0" name="Chef" placeholder="Chef" />
                         </div>
-                        <div className="flex flex-col  items-start w-full">
+                        <div className="flex flex-col items-start w-full gap-3">
                             <label>Supplier</label>
-                            <input type="text" className="p-4 rounded-md w-11/12 mr-0" name="Supplier" placeholder="Supplier" />
+                            <input type="text" className="p-4 rounded-md w-full md:w-11/12  mr-0" name="Supplier" placeholder="Supplier" />
                         </div>
-                        <div className="flex flex-col  items-start w-full">
+                        <div className="flex flex-col items-start w-full gap-3">
                             <label>Taste</label>
-                            <input type="text" className="p-4 rounded-md w-11/12 mr-0" name="Taste" placeholder="Taste" />
+                            <input type="text" className="p-4 rounded-md w-full md:w-11/12  mr-0" name="Taste" placeholder="Taste" />
                         </div>
-                        <div className="flex flex-col  items-start w-full">
+                        <div className="flex flex-col items-start w-full gap-3">
                             <label>Category</label>
-                            <input type="text" className="p-4 rounded-md w-11/12 mr-0" name="Category" placeholder="Category" />
+                            <input type="text" className="p-4 rounded-md w-full md:w-11/12  mr-0" name="Category" placeholder="Category" />
                         </div>
-                        <div className="flex flex-col  items-start w-full">
+                        <div className="flex flex-col items-start w-full gap-3">
                             <label>Details</label>
-                            <input type="text" className="p-4 rounded-md w-11/12 mr-0" name="Details" placeholder="Details" />
+                            <input type="text" className="p-4 rounded-md w-full md:w-11/12  mr-0" name="Details" placeholder="Details" />
                         </div>
                     </div>
-                    <div className="pr-10">
-                        <div className="flex flex-col  items-start w-full my-6">
+                    <div className="md:pr-10">
+                        <div className="flex flex-col items-start w-full gap-3 my-6">
                             <label>Photo</label>
                             <input type="text" className="p-4 rounded-md mr-0 w-full" name="Photo" placeholder="Photo" />
                         </div>
                     </div>
-                    <div className="pr-10 mt-12">
-                        <button className="btn btn-success w-full ">ADD COFFEE</button>
+                    <div className="md:pr-10 mt-12">
+                        <button className="btn btn-success w-full">ADD COFFEE</button>
 
                     </div>
                 </form>
